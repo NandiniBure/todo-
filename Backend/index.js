61221c0317e62234b2965ab59d16b2156ccae39e
NandiniBure <137141526+NandiniBure@users.noreply.github.com>
2024-11-28 21:57:40 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from your frontend
+    origin: "https://todo-73mb.vercel.app/", // Allow requests from your frontend
     credentials: true, // Allow cookies if needed
   })
 );
